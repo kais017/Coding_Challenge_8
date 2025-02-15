11bb90327e6b0ad354d4e318839cea4ce706256a
Your Name <your.email@example.com>
2025-02-15 16:05:46 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -11,3 +11,26 @@ console.log("Employee Salary Calculation")
 console.log(`Net Salary 1: ${calculateSalary(5000,500,0.1)}`)
 console.log(`Net Salary 2: ${calculateSalary(7000,1000,0.15)}`)
 // logging the test data
+
+// Task 2: Function Expression 
+
+function calculateDiscount(price,discountRate) {
+// creating a function that calculates the discount based on the price and discout rate
+
+let finalPrice = ((price)-(price * discountRate))
+{
+    return (finalPrice.toFixed(2))
+} // returns the function and uses .toFixed to include two decimal places in the output
+}
+
+console.log("Product Price After Discount")
+console.log(`Final Price 1: $${calculateDiscount(100, 0.2)}`);
+console.log(`Final Price 2: $${calculateDiscount(250, 0.15)}`);
+// logging each set of test data given and adding a title to the console log
+
+
+
+
+
+
+
