b35b718f67498e1ac726b68b472f73ba6f930a36
Your Name <your.email@example.com>
2025-02-15 16:17:47 -0500

    Task 3 - Service Fee Calculation

--- cc_8.js
+++ cc_8.js
@@ -28,6 +28,22 @@ console.log(`Final Price 1: $${calculateDiscount(100, 0.2)}`);
 console.log(`Final Price 2: $${calculateDiscount(250, 0.15)}`);
 // logging each set of test data given and adding a title to the console log
 
+// Task 3: Arrow Function
+const  calculateServiceFee = (amount,serviceType) => {
+// creating a function that calculates applies a fee
+    let serviceFee;
+ if (serviceType === "Premium") {serviceFee = 0.15;} // applies a 15%  fee for premium service
+else if (serviceType === "Standard") {serviceFee = 0.10;} // applies a 10% fee for standard service
+else if (serviceType === "Basic") {serviceFee = 0.05;} // applies a 5% fee for basic service
+
+return (amount * serviceFee).toFixed(2) // returns the service fee amount using .toFixed to include two decimal places
+}
+
+console.log("Service Fee Calculation") // adding a title to the console log
+console.log(`Service Fee 1: $${calculateServiceFee(200, "Premium")}`); // logging the Premium Service test data
+console.log(`Service Fee 1: $${calculateServiceFee(500, "Standard")}`); //loggin the Standard Service test data
+
+
 
 
 
